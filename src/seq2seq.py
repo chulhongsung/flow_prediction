e4a0f2077f6eda991960d88633b17e24c3577612
SungChul Hong <chulhongsung@gmail.com>
2020-08-29 20:59:54 +0900

    Update seq2seq.py

--- src/seq2seq.py
+++ src/seq2seq.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-class Encoder(tf.keras.models.Model):
+class Encoder(keras.layers.Layer):
     def __init__(self, d_model, l2_regularizer=0, dropout=0):
         super(Encoder, self).__init__()        
         self.lstm1 = keras.layers.LSTM(d_model,
@@ -29,7 +29,7 @@ class Encoder(tf.keras.models.Model):
         
         return lst_hidden2, lst_cell_state2
 
-class Decoder(tf.keras.models.Model):
+class Decoder(keras.layers.Layer):
     def __init__(self, d_model, k, l2_regularizer=0, dropout=0): 
         super(Decoder, self).__init__()
         self.k = k
@@ -60,7 +60,7 @@ class Decoder(tf.keras.models.Model):
                 
         return tf.squeeze(tf.transpose(tf.convert_to_tensor(output), perm=[1, 0, 2, 3]))
 
-class Seq2Seq(tf.keras.models.Model):
+class Seq2Seq(keras.models.Model):
     def __init__(self, d_model, k, l2_regularizer=0, dropout=0):
         super(Seq2Seq, self).__init__()
         self.encoder = Encoder(d_model, l2_regularizer, dropout)
